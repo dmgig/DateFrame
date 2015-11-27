1e5cce9983c0724bf831c4f43bd0e17fd3f04dd6
Dave M. Giglio <dave.m.giglio@gmail.com>
2015-11-27 12:19:02 -0500

    Update DateFrame.php

--- DateFrame.php
+++ DateFrame.php
@@ -1,9 +1,7 @@
 <?php
 
 /**
- * DateFrame class to ease generation of MySQL BETWEEN clauses
- * Contructor requires start and end date. Each DateFrame can then be broken into
- * arrays of shorter interval date frames of day, seven day, Sun-Sat weeks, or months.
+ * DateFrame class tfor working with date intervals and producting SQL BETWEEN statements
  */
 class DateFrame{
 	
